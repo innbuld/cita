4f93f0357d1035c1031ef48826d83edd5fdb5cb0
innbuld <kanmiolanrewaju@gmail.com>
2022-08-30 20:01:27 -0700

    commit

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const ANKR_API_KEY = process.env.ANKR_API_KEY;
+
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ANKR_API_KEY,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
 };
