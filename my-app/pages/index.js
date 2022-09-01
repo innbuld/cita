94b2e07e4d394ab80094d77159a6bf129da6cc61
innbuld <kanmiolanrewaju@gmail.com>
2022-09-01 16:36:42 -0700

    commit

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -7,3 +7,74 @@ import {abi, NFT_CONTRACT_ADDRESS } from "../constants";
 import styles from "../styles/Home.module.css";
 import navStyles from '../styles/Navbar.module.css'
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri"
+
+export default function Home() {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  // walletConnected keep track of whether the user's wallet is connected or not
+  const [walletConnected, setWalletConnected] = useState(false);
+  // presaleStarted keeps track of whether the presale has started or not
+  const [presaleStarted, setPresaleStarted] = useState(false);
+  // loading is set to true when we are waiting for a transaction to get mined
+  const [loading, setLoading] = useState(false);
+  // checks if the currently connected MetaMask wallet is the owner of the contract
+  const [isOwner, setIsOwner] = useState(false);
+  // tokenIdsMinted keeps track of the number of tokenIds that have been minted
+  const [tokenIdsMinted, setTokenIdsMinted] = useState("0");
+  // Create a reference to the Web3 Modal (used for connecting to Metamask) which persists as long as the page is open
+  const web3ModalRef = useRef();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
